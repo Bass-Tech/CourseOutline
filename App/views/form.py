5a88cd9241be91990d24d13791b8f057d3a5fe31
Marc-Anth <hostenmarc6@gmail.com>
2023-04-10 14:45:37 +0000

    form moved

--- App/views/form.py
+++ App/views/form.py
@@ -5,6 +5,11 @@ from App.models import document
 
 app = Flask(__name__)
 
+
+@app.route('/CompiledForm')
+def compiled_form():
+    return render_template('CompiledForm.html')
+
 @app.route('/App/views/form.py', methods=['POST'])
 def handle_form_data():
     data = request.get_json()
